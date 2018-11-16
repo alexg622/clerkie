5c6322e8c79f29efc8445dd98e25d332a19d9605
alexg622 <alex.gm62288@gmail.com>
2018-11-16 11:05:29 -0800

    Change get route name for transaction testing

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Transaction = require("../models/Transaction")
 const utils = require("../utils/utils")
 
-router.get("/transactions", async (req, res) => {
+router.get("/transactionsTesting", async (req, res) => {
   let answer = await utils.groupByName(Transaction)
   let newAnswer = await utils.parseByLength(answer)
   Transaction.find().then(trans => res.json(newAnswer))
@@ -23,9 +23,6 @@ module.exports = router
 
 
 // find all transactions with same name
+// in those find ones that have a length greater than 3
 // in all transactions that have the same name find similar pricing
 // in transactions with same name find consistant dates
-
-// Transaction.find().then(transact => {
-//   transact.map(trannil => trannil.remove())
-// })
